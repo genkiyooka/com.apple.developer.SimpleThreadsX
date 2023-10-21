e7b78b38016abad1c40f60797f77649d80bae44d
Gen Kiyooka <gen.kiyooka@yahoo.com>
2023-10-21 11:27:02 -0700

    Update Xcode 14

---
+++ SimpleThreadsX/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SimpleThreadsX
+//
+//  Created by dr000k music on 2023-10-18.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ SimpleThreadsX/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  SimpleThreadsX
+//
+//  Created by dr000k music on 2023-10-18.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (retain,nonatomic) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+	return YES;
+}
+
+
+@end
